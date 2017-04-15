a510b6d942368ff06e907dfe65b702056b4b4d84
lilianchang1234 <lilystar3636@gmail.com>
2017-04-15 11:25:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,32 @@
 // v3.1.0
 //Docs at http://simpleweatherjs.com
 $(document).ready(function() {
-$('#updateLocation').click(function () {
-  var location = $("input").val();
-  $.simpleWeather({
-    location: location,
-    woeid: '',
-    unit: 'f',
-    success: function(weather) {
-      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      html += '<li class="currently">'+weather.currently+'</li>';
-      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  $('#updateLocation').click(function () {
+    var location = $("input").val();
+    $.simpleWeather({
+      location: location,
+      woeid: '',
+      unit: 'f',
+      success: function(weather) {
+        html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+        html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+        html += '<li class="currently">'+weather.currently+'</li>';
+        html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
   
-      $("#weather").html(html);
-    },
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-   
+        $("#weather").html(html);
+      },
+      error: function(error) {
+        $("#weather").html('<p>'+error+'</p>');
+      }
+    });
   });
   
-});
+  var daysofWeek = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  var start = moment().day();
+  var len = daysOfWeek.length;
 
-});
-var daysofWeek = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var start = moment().day();
-var len = daysOfWeek.length;
-
-for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 5; i++) {
     $('#day' + i).text(daysOfWeek[start % len]);
     start++;
-}
+  }
+});

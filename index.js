d09d2dacc5b259f820a51d0d59039e46208fc6a4
lilianchang1234 <lilystar3636@gmail.com>
2017-05-13 10:56:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,24 @@
    $('#myModal').modal('show');
     }); 
 $(document).ready(function() {
-  $('#updateLocation').click(function () {
-    var location = $("input").val();
-    $.simpleWeather({
-      location: location,
-      woeid: '',
-      unit: 'f',
-      success: function(weather) {
-        html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-        html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-        html += '<li class="currently">'+weather.currently+'</li>';
-        html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+  $.simpleWeather({
+    location: 'Austin, TX',
+    woeid: '',
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
   
-        $("#weather").html(html);
-      },
-      error: function(error) {
-        $("#weather").html('<p>'+error+'</p>');
-      }
-    });
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
   });
+});
+
   
   var daysOfWeek = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   var start = moment().day();
